56a3a3d3dcb4334152bb2d173831cee3058fb1e3
diego-arapovic <diego.arapovic@gmail.com>
2023-01-06 18:30:23 +0100

    updated Diego exercise 3 solution

--- Sandbox/Diego/tutorial_exercise_3.py
+++ Sandbox/Diego/tutorial_exercise_3.py
@@ -12,15 +12,30 @@ eps_p = 0
 eps_u = 0
 
 #define max number of iterations
-max_iter = 5
+max_iter = 134
 
+# define g and its derivative
 def g(x):
     return x**2
 
 def dg_dx(x):
     return 2*x
 
+# run the simulation
 for i in range(0, max_iter):
+    # compute the new values into temporary variables
     phi_new = eps_u*dg_dx(phi) - eps_p
     eps_p_new = phi - v_p - eps_p*sigma_p
     eps_u_new = u - g(phi) - sigma_u*eps_u
+    
+    # update the actual variables
+    # eps_p = phi - v_p - eps_p_new
+    # eps_u = u - g(phi) - eps_u_new
+    phi = phi + 0.1 * phi_new
+    eps_p = eps_p + 0.1 * eps_p_new
+    eps_u = eps_u + 0.1 * eps_u_new    
+
+    print('--------' + str(i) + '-------')
+    print(phi)
+    print(eps_p)
+    print(eps_u)
